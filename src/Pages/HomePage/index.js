bb626ba05eec75f1648a19ce9a5e3eee9f88dba2
Aadil Ansari <aadil.ansari8@gmail.com>
2020-12-31 19:36:55 +0530

    added grid in Homepage

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,19 +1,21 @@
-import { makeStyles } from "@material-ui/core";
+import { Grid, makeStyles } from "@material-ui/core";
 import React from "react";
 import FavoriteIcon from "@material-ui/icons/Favorite";
+import computer_guy from "../../assets/computer_guy.png";
 
 const useHomeStyle = makeStyles({
   root: {
     width: "100vw",
     display: "flex",
     justifyContent: "center",
+    marginTop: 70,
   },
   container: {
     width: "70%",
   },
-  mainText:{
-    width:'30%'
-  }
+  mainText: {
+    // width: "35%",
+  },
 });
 
 const HomePage = () => {
@@ -21,9 +23,19 @@ const HomePage = () => {
   return (
     <div className={classes.root}>
       <div className={classes.container}>
-        <h1 className={classes.mainText}>
-          Hello, I’m Aadil Ansari and I <FavoriteIcon color='secondary' style={{ fontSize: 30 }}/> to build applications
-        </h1>
+        <Grid container>
+          <Grid md={6} xs={12} >
+            <h1 className={classes.mainText}>
+              Hello, I’m Aadil Ansari
+              <br /> and I{" "}
+              <FavoriteIcon color="secondary" style={{ fontSize: 30 }} /> to
+              build applications
+            </h1>
+          </Grid>
+          <Grid md={6} xs={12} >
+            <img src={computer_guy} alt="computer_guy" />
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
